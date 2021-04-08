cb0c2a8657b740bd2111dbdcebe7effd392ce1aa
Nalu e Matheus <NaluMatheus@MacBook-de-Nalu.local>
2021-04-08 14:51:13 -0300

    backend

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+connectDB();
+
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('Welcome to Contact Keeper API'));
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/users', require('./routes/users'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
