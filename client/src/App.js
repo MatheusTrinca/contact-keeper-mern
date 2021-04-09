03014af358887bfdda287145104cff11e2f74a45
Nalu e Matheus <NaluMatheus@MacBook-de-Nalu.local>
2021-04-08 23:58:37 -0300

    navbar e contact context

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Navbar from './layout/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import ContactState from './contexts/contact/ContactState';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <ContactState>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
+  );
 }
 
 export default App;
