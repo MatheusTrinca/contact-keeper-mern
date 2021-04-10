09307904bf2dcf34acaadaf94c26a4de2393ee73
MatheusTrinca <matheusfelipetrinca@gmail.com>
2021-04-09 23:12:13 -0300

    AuthState, AlertState, Register e Login

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,35 @@ import './App.css';
 import Navbar from './layout/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alerts from './layout/Alerts';
 import ContactState from './contexts/contact/ContactState';
+import AuthState from './contexts/auth/AuthState';
+import AlertState from './contexts/alert/AlertState';
 
 function App() {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
+      </ContactState>
+    </AuthState>
   );
 }
 
